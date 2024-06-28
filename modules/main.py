da99744c6b4fd148f6c69b0939d90e5a857d6956
Ashutosh Goswami <160889349+AshutoshGoswami24@users.noreply.github.com>
2024-06-29 02:22:56 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -36,7 +36,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("IAMASHU")
+    return web.json_response("https://github.com/AshutoshGoswami24")
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
